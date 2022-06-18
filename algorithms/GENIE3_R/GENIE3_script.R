58956b0a863008453e860040c6f228ac6398b4f0
Anna Ketteler <anna.ketteler@ownly.de>
2022-06-18 21:48:19 +0200

    write genie caller

--- algorithms/GENIE3_R/GENIE3_script.R
+++ algorithms/GENIE3_R/GENIE3_script.R
@@ -1,32 +1,16 @@
-
-source("/home/anna/BIONETS_code/algorithms/GENIE3_R/GENIE3.R") # TODO
+genie3_r <- dirname(rstudioapi::getSourceEditorContext()$path)
+source(paste(genie3_r, "/GENIE3.R", sep=""))
 
 args = commandArgs(trailingOnly=TRUE)
 data_path = args[0]
-regulators = args[1]
-targets = args[2]
+out_path = args[1]
+regulators = args[2]
+targets = args[3]
+#nTrees = args[4]
 treeMethod = "RF"
-K = "sqrt"
-nTrees = args[3]
-nCores = 1
-returnMatrix = TRUE
-verbose = FALSE
 
-exprMatrix <- read.expr.matrix(file_name=data_path, form="rows.are.samples", sep="\t")
+exprMat <- read.table(data_path, row.names=1, header = TRUE, sep='\t', as.is=TRUE)
+weightMat <- GENIE3(exprMat, nTrees=50, regulators=regulators, returnMatrix = TRUE, targets=targets, K=K, treeMethod=treeMethod)
+linkList <- get.link.list(weightMat)
 
-weightMat <- GENIE3(
-  exprMatrix,
-  regulators,
-  targets,
-  treeMethod,
-  K,
-  nTrees,
-  nCores,
-  returnMatrix,
-  verbose
-)
-
-linkList <- getLinkList(weightMat)
-dim(linkList)
-head(linkList)
-
+write.table(linkList, out_path, sep='\t')
