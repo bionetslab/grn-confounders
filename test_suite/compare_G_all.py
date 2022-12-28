821a947289e209260dd61855b6a058cb19bbdc78
Anna Ketteler <anna.ketteler@ownly.de>
2022-12-28 09:45:42 +0100

    put analysis scripts in new file

---
+++ test_suite/compare_G_all.py
@@ -0,0 +1,98 @@
+from scipy.stats import wilcoxon
+import Selectors
+import preprocessing
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def run_wilcoxon_tests(ct_sel, conf_sels, alg_sels, fro, to, rep_k, block_ids):
+    wilcox_fractions = pd.DataFrame(columns=['cohort', 'confounder/variable','method', 'bl0', 'bl1', 'frac'])
+    assert fro-to >= 20, 'Provide at least 20 networks.'
+    for conf_sel in conf_sels:
+        for alg_sl in alg_sels:
+            if conf_sel != Selectors.ConfounderSelector.NONE:
+                wilcox_fractions = pd.concat(wilcox_fractions, run_wilcoxon_test_single(ct_sel, conf_sel, alg_sel, fro, to, rep_k, block_ids))
+    wilcox_fractions.to_csv(os.path.join('results', ct_sel + '_wilcox_fractions.csv'))
+
+def run_wilcoxon_test_single(ct_sel, conf_sel, alg_sel, fro, to, rep_k, block_ids):
+    wilcox_fractions = pd.DataFrame(columns=['cohort', 'confounder/variable','method', 'bl0', 'bl1', 'frac'])
+    algorithm_wrapper = Selectors.get_algorithm_wrapper(Selectors.AlgorithmSelector(alg_sel))
+    print('Compute mean JIs for G_all and confounder-based networks...')
+    networks_blocks_conf = {bl: pd.DataFrame(columns=['size intersection', 'size union', 'state', 'k', 'mean JI', 'partID']) for bl in block_ids}
+    conf_results = {l:{bl:[] for bl in block_ids} for l in range(self.m_from, self.m_to)}
+    for j in range(fro, to):
+        plain = pd.read_csv(os.path.join(os.getcwd(), 'results', 'networks', 'conf_part'+str(j)+'_blockall_'+alg_sel+'_'+ct_sel+'_none_gene_list.csv'), header=0)
+        for bl in block_ids:
+            block_network = os.path.join(os.getcwd(), 'results', 'networks', 'conf_part'+str(j)+'_block'+bl+'_'+alg_sel+'_'+ct_sel+'_'+conf_sel+'_gene_list.csv')
+            algorithm_wrapper._inferred_networks = [plain, pd.read_csv(block_network, header=0)]
+            for k in rep_k:
+                ji, state, s_int, s_un = algorithm_wrapper.mean_jaccard_index_at_k(k)
+                conf_results[j][bl].append(ji)
+            JI_conf = pd.DataFrame({'k': rep_k, 'mean JI': conf_results[j][bl], 'partID':j,'confounder':conf_sel, 'method':alg_sel, 'cohort':ct_sel})
+            networks_blocks_conf[bl] = pd.concat(networks_blocks_conf[bl], JI_conf)
+
+    print('Compute mean JIs for G_all and random networks...')
+    networks_blocks_rnd = {bl: pd.DataFrame(columns=['size intersection', 'size union', 'state', 'k', 'mean JI', 'partID']) for bl in block_ids}
+    rnd_results = {l:{bl:[] for bl in block_ids} for l in range(fro, to)}
+    for j in range(fro, to):
+        plain = pd.read_csv(os.path.join(os.getcwd(), 'results', 'networks', 'conf_part'+str(j)+'_blockall_'+alg_sel+'_'+ct_sel+'_none_gene_list.csv'), header=0)
+        for bl in block_ids:
+            block_network = os.path.join(os.getcwd(), 'results', 'networks', 'rnd_part'+str(j)+'_block'+bl+'_'+alg_sel+'_'+ct_sel+'_'+conf_sel+'_gene_list.csv')
+            algorithm_wrapper._inferred_networks = [plain, pd.read_csv(block_network, header=0)]
+            for k in rep_k:
+                ji, state, s_int, s_un = algorithm_wrapper.mean_jaccard_index_at_k(k)
+                rnd_results[j][bl].append(ji)
+            JI_rnd = pd.DataFrame({'k': rep_k, 'mean JI': rnd_results[j][bl], 'partID':j, 'confounder':conf_sel,
+                                    'method':alg_sel, 'cohort':ct_sel})
+            networks_blocks_rnd[bl] = pd.concat(networks_blocks_rnd[bl], JI_rnd)
+
+    for bl in block_ids:
+        visualize_JIs(networks_blocks_conf[bl], networks_blocks_rnd[bl], bl)
+        sign_conf_rnd = 0
+        for k in rep_k:
+            rnd = networks_blocks_rnd[bl][networks_blocks_rnd[bl]['k'] == k]
+            conf = networks_blocks_conf[bl][networks_blocks_conf[bl]['k'] == k]
+            wilcox_ = wilcoxon(conf['mean JI'], rnd['mean JI'], alternative='less', correction=True)
+            if wilcox_.pvalue < 0.05:
+                sign_conf_rnd += 1
+        print(['One-sided wilcoxon test on ' + str(bl) + ' network and corresponding random network for ' +
+                    str(sign_conf_rnd/len(rep_k)) + ' of all tested k.'])
+
+    for bl0, bl1 in itt.product(block_ids, 2):
+        net0 = networks_blocks_conf[bl0]
+        net1 = networks_blocks_conf[bl1]
+        sign_conf_conf = 0
+        for k in rep_k:
+            block0_conf = net0[net0['k'] == k]
+            block1_conf = net1[net1['k'] == k]
+            try:
+                wilcox = wilcoxon(block0_conf['mean JI'],block1_conf['mean JI'], alternative='less', correction=True)
+                if wilcox.pvalue < 0.05:
+                    sign_conf_conf += 1
+            except:
+                assert all(x == 0.0 for x in (conf_0['mean JI']-conf_1['mean JI']))
+                continue
+        
+        wilcox_fractions = pd.concat([wilcox_fractions, pd.DataFrame({'cohort':ct_sel, 'confounder/variable':str(conf_sel), 
+                        'method':str(alg_sel), 'bl0':str(bl0), 'bl1':str(bl1), 'frac':sign_conf_conf/len(rep_k)})])
+    return wilcox_fractions
+
+def visualize_JIs(block_conf, block_rnd, bl, save=False):
+    plt.figure()
+    sns.set(font_scale=1.2)
+    sns.set_style('whitegrid')
+    block_conf['partition type'] = str(bl)
+    block_rnd['partition type'] = 'random with size\nof ' + str(bl) + ' block'
+    JI = pd.concat([block_conf, block_rnd])
+    g = sns.lineplot(JI, x='k', y='mean JI', hue='partition type', errorbar='sd')
+    g.set_title(alg_sel + ' ' + ct_sel + ' ' + conf_sel)
+    plt.legend(bbox_to_anchor=(1.02, 0.9), loc='upper left', title='block compared with $G^{all}$', borderaxespad=0)
+    if save:
+        g.figure.savefig(os.path.join(os.getcwd(), 'plots', alg_sel+'_'+conf_sel+'_'+ct_sel+'_compare_conf_and_rnd.pdf'))
+
+if __name__ == '__main__':
+    # TODO parser
+    run_wilcoxon_tests(ct_sel, conf_sels, alg_sels, fro, to, rep_k, block_ids)
+    
--- test_suite/test_runner.py
+++ test_suite/test_runner.py
@@ -5,8 +5,6 @@ import numpy as np
 import os
 import itertools as itt
 from scipy.stats import chi2_contingency
-from scipy.stats import mannwhitneyu
-from scipy.stats import wilcoxon
 from datetime import datetime
 from collections import OrderedDict
 
@@ -98,33 +96,15 @@ class TestRunner(object):
         self.rnd_results = {ct_sel: {conf_sel: {alg_sel: {i: list([]) for i in range(self.n_from, self.n_to)} 
             for alg_sel in self.algorithm_selectors} for conf_sel in self.confounder_selectors} for ct_sel in self.cancer_type_selectors}
         
-        # initilize data frame to store fraction of significant p-values in one-sided Mann-Whitney-U test
-        self.mwu_fractions = pd.DataFrame(columns=['cohort', 'confounder/variable', 'method', 'frac'])
-
     def run_on_cancer_types_confounders(self):
         """Runs the tests for a given cancer_type and confounder on all algorithms. Test is only performed if the partition induced
-        by a confounder contains more tahn one block.
+        by a confounder contains more tahn one block. Performs all requested chi2 tests.
         """
         for ct_sel in self.cancer_type_selectors:
             for conf_sel in self.confounder_selectors:
                 if len(list(self.conf_partitions[ct_sel][conf_sel].values())) > 1 or conf_sel == Selectors.ConfounderSelector.NONE:
-                    # chi^2 tests
                     self._run_chi2_tests(conf_sel, ct_sel)
                     self.run_on_all_cancer_types_confounders_partitions(ct_sel, conf_sel)
-
-            for conf_sel in self.confounder_selectors:
-                for alg_sl in self.algorithm_selectors:
-                    if conf_sel != Selectors.ConfounderSelector.NONE:
-                        print('Run one-sided Mann-Whitney-U test for each k...')
-                        self.run_mwu_tests(self, ct_sel, conf_sel, alg_sel)
-                    if conf_sel != Selectors.ConfounderSelector.NONE:
-                        print('Run Wilcoxon tests for each k...')
-                        self.run_wilcoxon_tests(ct_sel, conf_sel, alg_sel)
-                        
-            self.mwu_fractions.to_csv(os.path.join('results', self.stamp + '_mwu_fractions.csv'))
-            self.visualize_mwus(ct_sel)
-            if self.g_all:
-                self.wilcox_fractions.to_csv(os.path.join('results', self.stamp + '_wilcox_fractions.csv'))
 
     def run_on_all_cancer_types_confounders_partitions(self, ct_sel, conf_sel):
         """Runs the tests for a given cancer type and confounder with all specified algorithms.
@@ -181,127 +161,7 @@ class TestRunner(object):
                     pd.DataFrame({'size intersection': intersections, 'size union': unions, 'state': network_state, 'k': self.rep_k, 
                     'mean JI': self.rnd_results[ct_sel][conf_sel][alg_sel][i]}).to_csv(os.path.join('results', 'JI', 
                     f'rnd_{i}_{str(alg_sel)}_{str(conf_sel)}_{str(ct_sel)}_jaccInd.csv'), index=False)
-
-    def run_mwu_tests(self, ct_sel, conf_sel, alg_sel):
-        r = pd.concat([pd.DataFrame({'mean JI':rnd, 'k':self.rep_k}) for rnd in self.rnd_results[ct_sel][conf_sel][alg_sel].values()])
-        c = pd.concat([pd.DataFrame({'mean JI':conf, 'cohort':ct_sel, 'confounder/variable':str(conf_sel), 'method':alg_sel, 'k':self.rep_k}) for conf in self.conf_results[ct_sel][conf_sel][alg_sel].values()])
-        assert len(c) == len(r)
-        significants = 0
-        for k in self.rep_k:
-            r_k = r[r['k'] == k]
-            c_k = c[c['k'] == k]
-            mwu = mannwhitneyu(c_k['mean JI'],r_k['mean JI'], alternative='less')
-            if mwu.pvalue < 0.05:
-                significants += 1
-        self.mwu_fractions = pd.concat([self.mwu_fractions, pd.DataFrame({'cohort':ct_sel, 'confounder/variable':str(conf_sel), 
-                            'method':str(alg_sel), 'frac':significants/len(self.rep_k)})])
     
-    def run_wilcoxon_tests(self, ct_sel, conf_sel, alg_sel):
-        block_ids = list(self.conf_partitions[ct_sel][conf_sel].keys())
-        print('Compute mean JIs for G_all and confounder-based networks...')
-        networks_blocks_conf = {bl: pd.DataFrame(columns=['size intersection', 'size union', 'state', 'k', 'mean JI', 'partID']) for bl in block_ids}
-        conf_results = {l:{bl:[] for bl in block_ids} for l in range(self.m_from, self.m_to)}
-        for j in range(self.m_from, self.m_to):
-            plain = pd.read_csv(os.path.join(os.getcwd(), 'results', 'networks', 'conf_part'+str(j)+'_blockall_'+alg_sel+'_'+ct_sel+'_none_gene_list.csv'), header=0)
-            for bl in list(self.conf_partitions[ct_sel][conf_sel].keys()):
-                block_network = os.path.join(os.getcwd(), 'results', 'networks', 'conf_part'+str(j)+'_block'+bl+'_'+alg_sel+'_'+ct_sel+'_'+conf_sel+'_gene_list.csv')
-                inferred_networks = [plain, pd.read_csv(block_network, header=0)]
-                for k in self.rep_k:
-                    ji, state, s_int, s_un = mean_jaccard_index_at_k(k, inferred_networks, alg_sel)
-                    conf_results[j][bl].append(ji)
-                JI_conf = pd.DataFrame({'k': self.rep_k, 'mean JI': conf_results[j][bl]}, 'partID':j,'confounder':conf_sel, 
-                                        'method':alg_sel, 'cohort':ct_sel)
-                networks_blocks_conf[bl] = pd.concat(networks_blocks_conf[bl], JI_conf)
-
-        print('Compute mean JIs for G_all and random networks...')
-        networks_blocks_rnd = {bl: pd.DataFrame(columns=['size intersection', 'size union', 'state', 'k', 'mean JI', 'partID']) for bl in block_ids}
-        rnd_results = {l:{bl:[] for bl in block_ids} for l in range(self.n_from, self.n_to)}
-        for j in range(self.n_from, self.n_to):
-            plain = pd.read_csv(os.path.join(os.getcwd(), 'results', 'networks', 'conf_part'+str(j)+'_blockall_'+alg_sel+'_'+ct_sel+'_none_gene_list.csv'), header=0)
-            for bl in list(self.conf_partitions[ct_sel][conf_sel].keys()):
-                block_network = os.path.join(os.getcwd(), 'results', 'networks', 'rnd_part'+str(j)+'_block'+bl+'_'+alg_sel+'_'+ct_sel+'_'+conf_sel+'_gene_list.csv')
-                inferred_networks = [plain, pd.read_csv(block_network, header=0)]
-                for k in self.rep_k:
-                    ji, state, s_int, s_un = mean_jaccard_index_at_k(k, inferred_networks, alg_sel)
-                    rnd_results[j][bl].append(ji)
-                JI_rnd = pd.DataFrame({'k': self.rep_k, 'mean JI': rnd_results[j][bl]}, 'partID':j, 'confounder':conf_sel,
-                                        'method':alg_sel, 'cohort':ct_sel)
-                networks_blocks_rnd[bl] = pd.concat(networks_blocks_rnd[bl], JI_rnd)
-
-        for bl in block_ids:
-            self.visualize_JIs(networks_blocks_conf[bl], networks_blocks_rnd[bl], bl)
-            sign_conf_rnd = 0
-            for k in self.rep_k:
-                rnd = networks_blocks_rnd[bl][networks_blocks_rnd[bl]['k'] == k]
-                conf = networks_blocks_conf[bl][networks_blocks_conf[bl]['k'] == k]
-                wilcox_ = wilcoxon(conf['mean JI'], rnd['mean JI'], alternative='less', correction=True)
-                if wilcox_.pvalue < 0.05:
-                    sign_conf_rnd + = 1
-            self._log(['One-sided wilcoxon test on ' + str(bl) + ' network and corresponding random network for ' +
-                        str(sign_conf_rnd/len(self.rep_k)) + ' of all tested k.'])
-
-        for bl0, bl1 in itt.product(block_ids, 2):
-            net0 = networks_blocks_conf[bl0]
-            net1 = networks_blocks_conf[bl1]
-            sign_conf_conf = 0
-            for k in self.rep_k:
-                block0_conf = net0[net0['k'] == k]
-                block1_conf = net1[net1['k'] == k]
-                try:
-                    wilcox = wilcoxon(block0_conf['mean JI'],block1_conf['mean JI'], alternative='less', correction=True)
-                    if wilcox.pvalue < 0.05:
-                        sign_conf_conf + = 1
-                except:
-                    assert all(x == 0.0 for x in (conf_0['mean JI']-conf_1['mean JI']))
-                    continue
-            
-            self.wilcox_fractions = pd.concat([self.wilcox_fractions, pd.DataFrame({'cohort':ct_sel, 'confounder/variable':str(conf_sel), 
-                            'method':str(alg_sel), 'bl0':str(bl0), 'bl1':str(bl1), 'frac':sign_conf_conf/len(self.rep_k)})])
-
-    def visualize_mwus(ct_sel):
-        plt.tight_layout()
-        sns.set(font_scale=1.2)
-        sns.set_style("darkgrid")
-        palette={'age':"#2a60e1", 'ethnicity':"#e8e049", 'sex':"#69d447", 'WGCNA':'#15a5d9', 'CEMiTool':'#f9a62d', 'ARACNe-AP':'#d54f13'}
-        frms = set(self.mwu_fractions['cohort'].values))
-        h, axs_z = plt.subplots(frms,1, figsize=(14,5*frms), gridspec_kw=dict(width_ratios=[5], height_ratios=np.repeat(5, frms)))
-        h.tight_layout(pad=5.0)
-
-        for cohort in set(self.mwu_fractions['cohort'].values):
-            cur = self.mwu_fractions[self.mwu_fractions['cohort'] == cohort]
-            cur_var = pd.DataFrame({'method':cur['confounder/variable'], 'confounder/variable':cur['method'], 'frac':cur['frac']})
-            cur = pd.concat([cur, cur_var])
-
-            g =sns.swarmplot(ax=axs_z[i], x="method", y="frac",hue="confounder/variable",data=_cur, legend=True, s=15, palette=palette)
-            g.legend(loc='center left', bbox_to_anchor=(1.0, 0.5), ncol=1)
-            ax = g.axes
-            ax.yaxis.grid(True)
-            ax.xaxis.grid(True)
-            ax.axvline(2.5, ls='--')
-
-            ax.set_title(cohort, loc='left', fontdict={'fontsize': 28,'verticalalignment': 'baseline','horizontalalignment': 'left'})
-            g.set(xlabel='', ylabel = "fraction of k with \n significant p-value",title=cohort)
-            ax.yaxis.label.set_size(18)
-            g.tick_params(labelsize=16)
-            plt.yticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0])
-
-            i += 1
-
-        fig = g.get_figure()
-        h.savefig(os.path.join(cwd, 'mwu_comparison', str(cohort)+'_mwu_comparison.pdf'))
-
-    def visualize_JIs(block_conf, block_rnd, bl):
-        plt.figure()
-        sns.set(font_scale=1.2)
-        sns.set_style('whitegrid')
-        block_conf['partition type'] = str(bl)
-        block_rnd['partition type'] = 'random with size\nof ' + str(bl) + ' block'
-        JI = pd.concat([block_conf, block_rnd])
-        g = sns.lineplot(JI, x='k', y='mean JI', hue='partition type', errorbar='sd')
-        g.set_title(alg_sel + ' ' + ct_sel + ' ' + conf_sel)
-        plt.legend(bbox_to_anchor=(1.02, 0.9), loc='upper left', title='block compared with $G^{all}$', borderaxespad=0)
-        #g.figure.savefig(os.path.join(os.getcwd(), 'res_plots', alg_sel+'_'+conf_sel+'_'+ct_sel+'_plain_compar.pdf'))
-
     def save_networks(self, inferred_networks, part_nb, mode, alg_sel, ct_sel, conf_sel):
         """Saves the inferred networks to csv.
 
