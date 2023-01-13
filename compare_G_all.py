c9a85895d618f1706ed4eba9ab58d15930a41558
Anna Ketteler <anna.ketteler@ownly.de>
2023-01-13 14:44:57 +0100

    remove wilcoxon tests

--- compare_G_all.py
+++ compare_G_all.py
@@ -8,18 +8,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import argparse
 
-def run_wilcoxon_tests(ct_sel, conf_sels, alg_sels, fro, to, k, block_ids):
+def compute_and_plot_JIs(ct_sel, conf_sels, alg_sels, fro, to, k, block_ids):
+    print('Compute and plot JIs of G_all with random and confounder-linked networks of each block.')
     rep_k = range(10, k, 100)
-    wilcox_fractions = pd.DataFrame(columns=['cohort', 'confounder/variable','method', 'bl0', 'bl1', 'frac'])
-    #assert fro-to >= 20, 'Provide at least 20 networks.'
     for conf_sel in conf_sels:
         for alg_sel in alg_sels:
             if conf_sel != Selectors.ConfounderSelector.NONE:
-                wilcox_fractions = pd.concat([wilcox_fractions, run_wilcoxon_test_single(ct_sel, conf_sel, alg_sel, fro, to, rep_k, block_ids)])
-    wilcox_fractions.to_csv(os.path.join('results', ct_sel + '_wilcox_fractions.csv'))
+                compute_and_plot_JIs_single(ct_sel, conf_sel, alg_sel, fro, to, rep_k, block_ids)
 
-def run_wilcoxon_test_single(ct_sel, conf_sel, alg_sel, fro, to, rep_k, block_ids):
-    wilcox_fractions = pd.DataFrame(columns=['cohort', 'confounder/variable','method', 'bl0', 'bl1', 'frac'])
+def compute_and_plot_JIs_single(ct_sel, conf_sel, alg_sel, fro, to, rep_k, block_ids):
     algorithm_wrapper = Selectors.get_algorithm_wrapper(Selectors.AlgorithmSelector(alg_sel))
     print('Compute mean JIs for G_all and confounder-based networks...')
     networks_blocks_conf = {bl: pd.DataFrame(columns=['size intersection', 'size union', 'state', 'k', 'mean JI', 'partID']) for bl in block_ids}
@@ -50,36 +47,9 @@ def run_wilcoxon_test_single(ct_sel, conf_sel, alg_sel, fro, to, rep_k, block_id
                                     'method':alg_sel, 'cohort':ct_sel})
             networks_blocks_rnd[bl] = pd.concat([networks_blocks_rnd[bl], JI_rnd])
 
+    print('Plot JIs...')
     for bl in block_ids:
         visualize_JIs(networks_blocks_conf[bl], networks_blocks_rnd[bl], bl)
-        sign_conf_rnd = 0
-        for k in rep_k:
-            rnd = networks_blocks_rnd[bl][networks_blocks_rnd[bl]['k'] == k]
-            conf = networks_blocks_conf[bl][networks_blocks_conf[bl]['k'] == k]
-            wilcox_ = wilcoxon(conf['mean JI'], rnd['mean JI'], alternative='less', correction=True)
-            if wilcox_.pvalue < 0.05:
-                sign_conf_rnd += 1
-        print(['One-sided wilcoxon test on ' + str(bl) + ' network and corresponding random network for ' +
-                    str(sign_conf_rnd/len(rep_k)) + ' of all tested k.'])
-
-    for bl0, bl1 in itt.product(block_ids, 2):
-        net0 = networks_blocks_conf[bl0]
-        net1 = networks_blocks_conf[bl1]
-        sign_conf_conf = 0
-        for k in rep_k:
-            block0_conf = net0[net0['k'] == k]
-            block1_conf = net1[net1['k'] == k]
-            try:
-                wilcox = wilcoxon(block0_conf['mean JI'],block1_conf['mean JI'], alternative='less', correction=True)
-                if wilcox.pvalue < 0.05:
-                    sign_conf_conf += 1
-            except:
-                assert all(x == 0.0 for x in (conf_0['mean JI']-conf_1['mean JI']))
-                continue
-        
-        wilcox_fractions = pd.concat([wilcox_fractions, pd.DataFrame({'cohort':ct_sel, 'confounder/variable':str(conf_sel), 
-                        'method':str(alg_sel), 'bl0':str(bl0), 'bl1':str(bl1), 'frac':sign_conf_conf/len(rep_k)})])
-    return wilcox_fractions
 
 def visualize_JIs(block_conf, block_rnd, bl, save=False):
     plt.figure()
@@ -97,7 +67,7 @@ def visualize_JIs(block_conf, block_rnd, bl, save=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
                     prog = 'compare_g_all',
-                    description = 'Compare networks inferred from blocks with G_all, then compare netorks from random blocks with G_all. First, use Wilcoxon test to determine for each block whether the distributions of random-based and confounder-based mean JIs differ significantly. Then, determine whether one of the confounder-based networks compares significantly less to G_all than the other networks.')
+                    description = 'Compare networks inferred from blocks with G_all, then compare netorks from random blocks with G_all, by computing mean JIs. Then plot the JIs.')
     parser.add_argument('-ct', required=True)
     parser.add_argument('-conf', required=True, nargs='+')
     parser.add_argument('-alg', required=True, nargs='+', choices=[str(sel) for sel in list(Selectors.AlgorithmSelector)])
@@ -107,5 +77,5 @@ if __name__ == '__main__':
     parser.add_argument('-block_ids', required=True, nargs='+')
     args = parser.parse_args()
     assert args.fro < args.to
-    run_wilcoxon_tests(args.ct, args.conf, args.alg, args.fro, args.to, args.k, args.block_ids)
+    compute_and_plot_JIs(args.ct, args.conf, args.alg, args.fro, args.to, args.k, args.block_ids)
     
