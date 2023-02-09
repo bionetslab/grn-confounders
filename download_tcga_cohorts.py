0bb2bb300ea73061492b73b25eec54b96a027cf2
Anna Ketteler <anna.ketteler@ownly.de>
2023-02-09 16:21:17 +0100

    fix typo in print

--- download_tcga_cohorts.py
+++ download_tcga_cohorts.py
@@ -9,7 +9,10 @@ if __name__ == '__main__':
     parser.add_argument('-ct', required=True, nargs='+', help='Use TCGA study abbreviations to specify the cohorts to be downloaded')
     args = parser.parse_args()
     for ct_sel in list(args.ct):
+        print(f'downloading gene expression data for {ct_sel}. This can take a few minutes...')
         Downloader.download_TCGA_expression_data(ct_sel)
+        print(f'downloading pheno type data for {ct_sel}. This can take a few minutes...')
         Downloader.download_TCGA_phenotype_data(ct_sel)
     if args.tfs:
+        print('downloading list of known transcription factors...')
         Downloader.download_known_tfs()
