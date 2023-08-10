389066323cc35ebd4393182be3706fad7e3576bd
Anna Ketteler <anna.ketteler@ownly.de>
2023-08-10 20:45:23 +0200

    add download and preprocessing of metabric data

---
+++ download_test_data.py
@@ -0,0 +1,27 @@
+from confinspect import Downloader
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+                    prog = 'download_TCGA_cohorts',
+                    description = 'Download TCGA gene expression data and phenotype data and list of knon transcription factors as list of regulators for GRN methods.')    
+    parser.add_argument('-ct', required=False, nargs='*', help='Use TCGA study abbreviations to specify the cohorts to be downloaded. If not set, all cohorts featured in our paper will be downloaded.')
+    args = parser.parse_args()
+
+    if not args.ct:
+        ct_sels = ['brca_metabric', 'BRCA', 'CESC', 'COAD', 'GBM', 'HNSC', 'KIRC', 'KIRP', 'LUSC', 'PCPG', 'READ', 'STAD']
+    else:
+        ct_sels = list(args.ct)
+
+    for ct_sel in ct_sels:
+        print(f'downloading gene expression data and phenotype data for {ct_sel}. This can take a few minutes...')
+        if ct_sel == 'brca_metabric':
+            Downloader.download_metabric_data()
+        else:
+            Downloader.download_TCGA_expression_data(ct_sel)
+            Downloader.download_TCGA_phenotype_data(ct_sel)
+
+    print('downloading list of known transcription factors...')
+    Downloader.download_known_tfs()
+    print('downloading list of protein-coding genes...')
+    Downloader.download_protein_coding_genes()
--- src/confinspect/Downloader.py
+++ src/confinspect/Downloader.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import os
 import argparse
 from . import Selectors
@@ -39,6 +40,47 @@ def download_TCGA_phenotype_data(cancer_type_selector):
             os.mkdir(os.path.join(cwd, 'data'))
         pheno_data.to_csv(os.path.join(cwd, 'data', 'TCGA-'+str(cancer_type_selector)+'.GDC_phenotype.tsv'), sep='\t')
 
+def download_metabric_data():
+    """Saves metabric expression data and phenotype data for metabric brca in /data.
+    """
+    cwd = os.getcwd()
+    if not os.path.exists(os.path.join(cwd, 'data')):
+        os.mkdir(os.path.join(cwd, 'data'))
+    url = 'https://media.githubusercontent.com/media/cBioPortal/datahub/master/public/brca_metabric/data_mrna_illumina_microarray.txt'
+    df = pd.read_csv(url, delimiter='\t', index_col='Hugo_Symbol')
+
+    # filter and translate gene identifiers to ensembl id
+    df = df.drop('Entrez_Gene_Id', axis=1)
+    df.index.name = None
+    df = df.transpose()
+    pcgs = pd.read_csv('http://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/locus_groups/protein-coding_gene.txt', delimiter='\t', index_col=False, dtype=str)
+    pcgs = pcgs[['ensembl_gene_id', 'symbol']]
+    df = df[[col for col in pcgs['symbol'].values if col in df.columns]]
+    df = df.loc[:,~df.columns.duplicated()].copy()
+    assert pd.Series(df.columns).is_unique
+    df = df.rename(columns=dict(zip(pcgs.symbol, pcgs.ensembl_gene_id)))
+    df = df.dropna()
+    # get pheno data of patients
+    url = 'https://media.githubusercontent.com/media/cBioPortal/datahub/master/public/brca_metabric/data_clinical_patient.txt'
+    pheno = pd.read_csv(url, sep='\t', index_col=0)
+    pheno = pheno.drop(['#Identifier to uniquely specify a patient.', '#STRING', '#1', 'PATIENT_ID'])
+    # get pheno data of samples
+    url = 'https://media.githubusercontent.com/media/cBioPortal/datahub/master/public/brca_metabric/data_clinical_sample.txt'
+    spl = pd.read_csv(url, sep='\t', index_col=0)
+    spl = spl.drop(['#Identifier to uniquely specify a patient.', '#STRING', '#1', 'PATIENT_ID'])
+    spl = spl[spl['Sample Type'] == 'Primary']
+    # merge pheno data of samples and patients and make sure that there is only one sample per patient
+    pheno = pd.merge(pheno, spl, left_index=True, right_index=True)
+    assert pd.Series(pheno['Sample Identifier']).is_unique
+    assert pd.Series(pheno.index).is_unique
+    pheno = pheno.set_index('Sample Identifier')
+    # add age_quartile column according to age quartiles in TCGA-BRCA data (values taken from paper)
+    pheno = pheno[(pheno['Age at Diagnosis'].astype(float) <= 49) | (pheno['Age at Diagnosis'].astype(float) > 67)]
+    pheno['age_quartile'] = np.where(pheno['Age at Diagnosis'].astype(float) > 67, 'upper', 'lower')
+    # save expression data and phenotype data
+    pheno.to_csv(os.path.join(cwd, 'data', 'metabric_brca_pheno.tsv'), sep='\t')
+    df.to_csv(os.path.join(cwd, 'data', 'metabric_brca.tsv'), sep='\t')
+
 def download_known_tfs():
     """Saves known human transcription factors obtained from humantfs.ccbr.utoronto.ca in /data.
     """
@@ -46,7 +88,7 @@ def download_known_tfs():
     if not os.path.exists(os.path.join(cwd, 'data')):
         os.mkdir(os.path.join(cwd, 'data'))
     df = pd.read_csv('http://humantfs.ccbr.utoronto.ca/download/v_1.01/TFs_Ensembl_v_1.01.txt', delimiter='\t', index_col=0)
-    df.to_csv(os.path.join(cwd, 'data', 'regulators_.csv'), sep=',')
+    df.to_csv(os.path.join(cwd, 'data', 'regulators.csv'), sep=',')
 
 def download_protein_coding_genes():
     """Saves protein coding genes obtained from http://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/locus_groups/protein-coding_gene.txt
